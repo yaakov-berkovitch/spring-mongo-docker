935cb20a69c2de6428a5160a63df19dcf4069cbe
yaakov-berkovitch <yaakov.berkovitch@gmail.com>
2018-01-16 12:16:29 +0200

    Update Application.java

--- src/main/java/org/demo/springmongo/Application.java
+++ src/main/java/org/demo/springmongo/Application.java
@@ -1,1 +1,61 @@
+package org.demo.springmongo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@ComponentScan({"org.demo.springmongo"})
+@EnableMongoRepositories({"org.demo.springmongo"})
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Application implements CommandLineRunner {
+	@Autowired
+	private ApplicationConfig appConfig;
+	
+	@Autowired
+	private UserRepository repository;
+
+	public static void main(String[] args) {
+//        AbstractApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
+		c.setIgnoreUnresolvablePlaceholders(true);
+
+		return c;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+//		repository.deleteAll();
+
+		// save a couple of customers
+		repository.save(new User("zz1@gmail.com", "zz1"));
+		repository.save(new User("zz2@gmail.com", "zz2"));
+
+		// fetch all customers
+		System.out.println("Users found with findAll():");
+		System.out.println("-------------------------------");
+		for (User user : repository.findAll()) {
+			System.out.println(user);
+		}
+		System.out.println();
+
+		// fetch an individual customer
+		System.out.println("Customer found with findByFirstName('Alice'):");
+		System.out.println("--------------------------------");
+		System.out.println(repository.findByEmail("zz1@gmail.com"));
+	}
+
+}
 
